b9fd01e9edd780344e26738ca637ae37fc98af08
patrick <tpdud285@gmail.com>
2022-10-18 00:26:50 +0900

    ADD 1문제

--- src/programmers/level2/점프와순간이동.java
+++ src/programmers/level2/점프와순간이동.java
@@ -1,4 +1,15 @@
 package programmers.level2;
 
 public class 점프와순간이동 {
+    public int solution(int n) {
+        int ans = 0;
+
+        while(n > 0){
+            if(n % 2 != 0) ans++;
+            n /= 2;
+        }
+
+
+        return ans;
+    }
 }
