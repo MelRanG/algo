20bdcc7960736dcb4166ab8ba9b830d2933d3c39
Patrick <62234293+MelRanG@users.noreply.github.com>
2023-04-19 15:04:08 +0900

    [Silver III] Title: 진우의 달 여행 (Small), Time: 124 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.java
+++ 백준/Silver/17484. 진우의 달 여행 （Small）/진우의 달 여행 （Small）.java
@@ -1,49 +1,52 @@
 import java.io.*;
 import java.util.*;
-public class Main {
+class Main {
+
+    static int N,M;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
         int[][] arr = new int[N][M];
         int[][][] dp = new int[N][M][3];
-
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
         for (int i = 0; i < M; i++) {
-            for (int d = 0; d < 3; d++) {
-                dp[0][i][d] = arr[0][i];
+            for (int j = 0; j < 3; j++) {
+                dp[0][i][j] = arr[0][i];
             }
         }
-
         for (int i = 1; i < N; i++) {
+            dp[i][0][0] = Integer.MAX_VALUE;
+            dp[i][M-1][2] = Integer.MAX_VALUE;
             for (int j = 0; j < M; j++) {
-                for (int d = 0; d < 3; d++) {
-                    int newRow = i - 1;
-                    int newCol = j + d - 1;
-
-                    if (newCol >= 0 && newCol < M) {
-                        dp[i][j][d] = dp[newRow][newCol][(d + 1) % 3] + arr[i][j];
-                        dp[i][j][d] = Math.min(dp[i][j][d], dp[newRow][newCol][(d + 2) % 3] + arr[i][j]);
-                    } else {
-                        dp[i][j][d] = 1000 * 1000 + 1; // 초기값을 매우 큰 값으로 설정
-                    }
+                if(j-1 < 0){
+                    dp[i][j][1] = Math.min(dp[i-1][j][0], dp[i-1][j][2]) + arr[i][j];
+                    dp[i][j][2] = Math.min(dp[i-1][j+1][0], dp[i-1][j+1][1]) + arr[i][j];
+                }else if(j + 1 >= M){
+                    dp[i][j][1] = Math.min(dp[i-1][j][0], dp[i-1][j][2]) + arr[i][j];
+                    dp[i][j][0] = Math.min(dp[i-1][j-1][2], dp[i-1][j-1][1]) + arr[i][j];
+                }else{
+                    dp[i][j][0] = Math.min(dp[i-1][j-1][2], dp[i-1][j-1][1]) + arr[i][j];
+                    dp[i][j][1] = Math.min(dp[i-1][j][0], dp[i-1][j][2]) + arr[i][j];
+                    dp[i][j][2] = Math.min(dp[i-1][j+1][0], dp[i-1][j+1][1]) + arr[i][j];
                 }
             }
         }
-
         int result = Integer.MAX_VALUE;
         for (int i = 0; i < M; i++) {
-            for (int d = 0; d < 3; d++) {
-                result = Math.min(result, dp[N - 1][i][d]);
+            for (int j = 0; j < 3; j++) {
+                result = Math.min(result,dp[N-1][i][j]);
+
             }
         }
         System.out.println(result);
     }
+
 }
