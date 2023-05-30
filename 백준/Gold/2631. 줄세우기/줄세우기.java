e843d009d3b15c3573df5789449e4bceab07b7ba
Patrick <62234293+MelRanG@users.noreply.github.com>
2023-05-30 15:09:20 +0900

    [Gold IV] Title: 줄세우기, Time: 120 ms, Memory: 14040 KB -BaekjoonHub

--- 백준/Gold/2631. 줄세우기/줄세우기.java
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -1,27 +1,27 @@
+import java.io.*;
+import java.util.*;
+class Main {
+    static int N;
 
-
-import java.util.Scanner;
-
-public class Main {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] input = new int[N];
-        for(int i=0;i<N;i++) input[i] = sc.nextInt();
-
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
         int[] dp = new int[N];
         dp[0] = 1;
-        
-        int result = 0;
+        int max = 0;
         for (int i = 1; i < N; i++) {
-        	dp[i] = 1;
-			for (int j = 0; j < i; j++) {
-				if(input[j] < input[i]) dp[i] = Math.max(dp[i], dp[j]+1);
-			}
-			result = Math.max(result, dp[i]);
-		}
-        
-        System.out.println(N - result);
-	}
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if(arr[j] < arr[i]) dp[i] = Math.max(dp[i], dp[j]+1);
+            }
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(N-max);
+    }
+
 }
+
